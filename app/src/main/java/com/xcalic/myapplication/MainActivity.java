c1dd8680341b467442eb4c73a9b36e1b351604ea
nekechs <baradmayank>
2020-02-29 15:07:33 -0500

    yep

--- app/src/main/java/com/xcalic/myapplication/MainActivity.java
+++ app/src/main/java/com/xcalic/myapplication/MainActivity.java
@@ -2,13 +2,72 @@ package com.xcalic.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView isOnText;
+    Switch btToggle;
+    Button discover, pairNew;
+
+    BluetoothAdapter mBlueAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bluetooth_pairscreen);
+
+        btToggle = findViewById(R.id.btToggle);
+        discover = findViewById(R.id.discover);
+        isOnText = findViewById(R.id.isPaired);
+        pairNew = findViewById(R.id.getDevice);
+
+        btToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked) {
+                    if(!mBlueAdapter.isEnabled()) {
+                        showToast("Turning on bluetooth...");
+
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    }
+
+                }
+
+                else {
+
+                }
+
+            }
+        });
+
+        discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        pairNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
